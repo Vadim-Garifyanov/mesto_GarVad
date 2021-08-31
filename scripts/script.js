2d1c0c0e8b09365c4331c6e88356ec861a1c9efa
Vadim Garifyanov <z-z-z98@mail.ru>
2021-08-31 20:38:11 +0300

    error correction-2

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,24 @@
 let page = document.querySelector('.page');
 let buttonEdit = page.querySelector('.profile__edit');
-let formElement = page.querySelector('.popup');
+let formElement = page.querySelector('.popup__content');
+let popup = page.querySelector('.popup');
 let buttonClose = page.querySelector('.popup__close');
 let profName = page.querySelector('.profile__info-name');
 let profJob = page.querySelector('.profile__info-job');
-let nameInput = page.querySelector('input[name="name_edit"]');
-let jobInput = page.querySelector('input[name="job_edit"]');
+let nameInput = page.querySelector('input[name="name-edit"]');
+let jobInput = page.querySelector('input[name="job-edit"]');
 
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
 
 function openPopup() {
-  formElement.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
   nameInput.value = profName.textContent;
   jobInput.value = profJob.textContent;
 }
 
 function closePopup() {
-  formElement.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function handleProfileFormSubmit (evt) {
